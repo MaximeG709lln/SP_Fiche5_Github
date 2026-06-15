ec516e8067997ab85c872658bbee259ee6780432
MaximeG709lln <MaximeG709@LLN.CampusSintUrsula.be>
2026-06-15 10:34:24 +0200

    Added class Personage (attributes: name, lvl, role) + info program start/end

--- SP_Fiche5_Versiebeheer/Program.cs
+++ SP_Fiche5_Versiebeheer/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine($"SP Program - Versiebeheer Github:");
+Personage gys = new Personage();
+gys.ShowInfo();
+
+Console.WriteLine("(Program end - Versiebeheer)");
+
+class Personage
+{
+    string name = "Gys";
+    int lvl = 10;
+    string role = "Knight";
+
+    public void ShowInfo()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Player's information:");
+        Console.WriteLine("-----------------------");
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Level: {lvl}");
+        Console.WriteLine($"Role: {role}");
+        Console.WriteLine("-----------------------");
+        Console.ResetColor();
+    }
+}
